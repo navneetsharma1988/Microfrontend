b69293812a630858010385ca3c0c18763d562f00
Navneet Sharma <navneet.sharma@nagarro.com>
2021-10-16 21:10:09 +0530

    Update container App

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,7 +3,7 @@ import MarketingApp from "./components/MarketingApp";
 
 export default () => {
     return <div>
-        <h1>Hi I am Container!</h1>
+        <h1>Hi I am Container App!</h1>
         <hr/>
         <MarketingApp />
     </div>
