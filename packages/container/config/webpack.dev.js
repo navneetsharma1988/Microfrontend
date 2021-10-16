def4fae7df752f3a0947b136fa953012a89c5512
Navneet Sharma <navneet.sharma@nagarro.com>
2021-10-16 19:27:17 +0530

    Created container deploy script

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,5 +1,4 @@
 const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const commonConfig = require('./webpack.common');
 const packageJson = require("../package.json");
@@ -20,9 +19,7 @@ const devConfig = {
            },
            shared: packageJson.dependencies
         }),
-        new HtmlWebpackPlugin({
-            template: './public/index.html'
-        })
+
     ]
 }
 
