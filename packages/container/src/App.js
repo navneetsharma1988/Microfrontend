37a422786c14ef841cd86b156bfeabc4ded92772
Navneet Sharma <navneet.sharma@nagarro.com>
2021-10-17 13:18:18 +0530

    fix style collision issue

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,16 +1,23 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
 
 import Header from './components/header/Header';
 import MarketingApp from "./components/MarketingApp";
 
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co'
+});
+
 export default () => {
     return (
     <BrowserRouter>
-        <div>
-            <Header />
-            <MarketingApp />
-        </div>
+        <StylesProvider generateClassName={generateClassName}>
+            <div>
+                <Header />
+                <MarketingApp />
+            </div>
+        </StylesProvider>
     </BrowserRouter>
     )
 
--- packages/marketing/src/components/Landing.js
+++ packages/marketing/src/components/Landing.js
@@ -65,7 +65,6 @@ const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 export default function Album() {
   const classes = useStyles();
-
   return (
     <React.Fragment>
       <main>
